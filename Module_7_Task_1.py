c1506388ec15702e61d270d823164367bac378c8
Maryna Velychynska <marien5@ukr.net>
2023-01-26 18:05:56 +0100

    Module_7_Task_1.py

--- Module_7_Task_1.py
+++ Module_7_Task_1.py
@@ -38,38 +38,25 @@ class BusinessContact(Card):
       return f"{self.name} {self.email} {self.phone_number} {self.job} {self.company}"
 
      def contact(self):
-        print(f"Я телефоную по номеру {self.phone_number} та звязуюсь з {self.name}")
+        print(f"Я телефоную по номеру {self.phone_number} та звязуюсь з {self.name}") 
 
 from faker import Faker
 faker = Faker()
 
-card = BaseContact(name = faker.name(), email = faker.email(), phone_number = faker.phone_number())
-
-card_2 = BusinessContact(name = faker.name(), email = faker.email(), job = faker.job(), company = faker.company(), phone_number = faker.phone_number())
-
+card = Card(name = faker.name(), email = faker.email(), phone_number = faker.phone_number())
 card = []
-card_2 = []
 
-def create_contact(quantity:int,type):
+def create_contact(quantity:int):
  for i in range(quantity):
 
-   card.append(BaseContact(name = faker.name(), email = faker.email(), phone_number = faker.phone_number()))
+   card.append(Card(name = faker.name(), email = faker.email(), phone_number = faker.phone_number()))
 
-   card_2.append(BusinessContact(name = faker.name(), email = faker.email(), job = faker.job(), company = faker.company(), phone_number = faker.phone_number()))
- 
- if type == "1": 
-  for i in range(quantity):
-   if quantity > 0:
+ for i in range(quantity):
+  if quantity > 0:
     print (str(card[i]))
 
- elif type =="2":
-  for i in range(quantity):
-   if quantity > 0:
-    print (str(card_2[i]))
-
 if __name__ == "__main__": 
- type = input("Оберіть тип візитки:\n1. Особиста\n2. Робоча\n")
  quantity = int(input("Введіть кількість контактів: "))
 
- create_contact(quantity,type)
+ create_contact(quantity)
     
